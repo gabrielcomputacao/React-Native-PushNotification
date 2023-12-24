005fda1630d71adbf59175135ac91120957fea80
gabriel <gabrielcomputacao@outlook.com>
2023-12-24 12:52:10 -0300

    refactor: installing push notifications with oneSignal

--- App.tsx
+++ App.tsx
@@ -12,9 +12,16 @@ import { THEME } from "./src/theme";
 import { Loading } from "./src/components/Loading";
 
 import { CartContextProvider } from "./src/contexts/CartContext";
+import { OneSignal } from "react-native-onesignal";
+
+OneSignal.initialize("1d935d9f-e08b-46c7-937e-19216a1488b8");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  // useEffect(() => {
+  //   onesignalInitialize();
+  // }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>
