51553ddb278d619a1fd2fc77156858d11f1c8a5a
gabriel <gabrielcomputacao@outlook.com>
2023-12-26 09:10:14 -0300

    refactor: notifications with tags using oneSignal

--- App.tsx
+++ App.tsx
@@ -13,12 +13,15 @@ import { Loading } from "./src/components/Loading";
 
 import { CartContextProvider } from "./src/contexts/CartContext";
 import { OneSignal } from "react-native-onesignal";
+import { tagUserEmailCreate } from "./src/notifications/notificationsTags";
 
 OneSignal.initialize("1d935d9f-e08b-46c7-937e-19216a1488b8");
 OneSignal.User.addEmail("gabrielcomputacao@outlook.com");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate("gabrielcomputacao@outlook.com");
 
   // useEffect(() => {
   //   onesignalInitialize();
