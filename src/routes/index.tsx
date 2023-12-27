eabab70b7febb2836abb052099ba001f42b0f6c1
gabriel <gabrielcomputacao@outlook.com>
2023-12-27 09:42:43 -0300

    feat: using deep linking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,27 @@
-import { useTheme } from 'native-base';
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { useTheme } from "native-base";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 
-import { AppRoutes } from './app.routes';
+import { AppRoutes } from "./app.routes";
+
+/* configurando deep linking, quando clica em uma notificação o app abre a aplicação passando parametro ou não */
+
+const linking = {
+  prefixes: [
+    "igniteshoesapp://",
+    "com.rocketseat.igniteshoesapp://",
+    "exp+igniteshoesapp://",
+  ],
+  config: {
+    screens: {
+      details: {
+        path: "details/:productId",
+        parse: {
+          productId: (productId: string) => productId,
+        },
+      },
+    },
+  },
+};
 
 export function Routes() {
   const { colors } = useTheme();
@@ -10,7 +30,7 @@ export function Routes() {
   theme.colors.background = colors.gray[700];
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
     </NavigationContainer>
   );
